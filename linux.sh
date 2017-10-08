cb903246627c1c4166537dd49c8fb82527f859b3
Kalyan Veeramachaneni <kveerama.su@gmail.com>
2017-10-08 18:17:29 -0400

    updates

--- linux.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# OSX Installation
-sudo apt-get install build-essential
-sudo pip install -r requirements.txt
--- osx.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# OSX Installation
-brew install gcc@5 --without-multilib
-sudo pip install -r requirements.txt
