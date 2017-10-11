3bf4b974bbcb367a5c4b40d792847aeb2032c00b
Gaurav Sheni <gvsheni@gmail.com>
2017-10-11 18:55:40 -0400

    fixed linux install

--- install_linux.sh
+++ install_linux.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # Linux Installation
-sudo apt-get update
-sudo apt-get install python-pip python-dev build-essential
-pip install --upgrade --user pip
+sudo apt-get -y update
+sudo apt-get -y install python-pip python-dev build-essential
+sudo apt-get -y install python-setuptools
+sudo pip install --upgrade --force-reinstall pip==9.0.1
 pip install --user virtualenv virtualenvwrapper
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install gcc-5 g++-5
 virtualenv venv
 .venv/bin/activate
 pip install -r requirements.txt
