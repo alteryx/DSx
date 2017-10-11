097d36d41f438d9be221365bdb3e27eac3347507
Gaurav Sheni <gvsheni@gmail.com>
2017-10-11 18:58:37 -0400

    -y

--- install_linux.sh
+++ install_linux.sh
@@ -6,8 +6,8 @@ sudo apt-get -y install python-setuptools
 sudo pip install --upgrade --force-reinstall pip==9.0.1
 pip install --user virtualenv virtualenvwrapper
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-get update
-sudo apt-get install gcc-5 g++-5
+sudo apt-get -y update
+sudo apt-get -y install gcc-5 g++-5
 virtualenv venv
 .venv/bin/activate
 pip install -r requirements.txt
