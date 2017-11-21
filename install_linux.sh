89170455d84e21350bcb972931643c9a8abd4d74
Max Kanter <kmax12@gmail.com>
2017-11-21 00:40:14 -0500

    edits

--- install_linux.sh
+++ install_linux.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-# OSX Installation
-sudo apt-get install build-essential
-sudo pip install -r requirements.txt
+# Linux Installation
+sudo apt-get -y update
+sudo apt-get -y install python-pip python-dev build-essential
+sudo apt-get -y install python-setuptools
+sudo pip install --upgrade --force-reinstall pip==9.0.1
+pip install --user virtualenv virtualenvwrapper
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get -y update
+sudo apt-get -y install gcc-5 g++-5
+virtualenv venv
